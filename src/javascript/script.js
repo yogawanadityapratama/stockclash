7138dcf7bc1f8d1b82fdb21aa983fd92d437a841
Yogawan Aditya Pratama <123430193+yogawanadityapratama@users.noreply.github.com>
2023-12-06 19:05:13 +0700

    mengubah konten

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -18,3 +18,22 @@ function hideAllSections() {
     section.style.display = "none";
   });
 }
+
+// const sr = ScrollReveal({
+//   origin: 'top',
+//   distance: '60px',
+//   duration: 2500,
+//   delay: 400
+// })
+
+// sr.reveal('.nav')
+// sr.reveal('.nav', {delay: 500})
+// sr.reveal('.logo', {delay: 600})
+
+// sr.reveal('.hero', {delay: 700})
+// sr.reveal('.hero-button', {interfal: 100, delay: 700})
+
+// sr.reveal('.card', {delay: 800})
+
+// sr.reveal('.content', {delay: 900})
+// sr.reveal('.filters', {delay: 900})
